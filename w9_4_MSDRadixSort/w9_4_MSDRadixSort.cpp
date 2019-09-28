1b79e34a3dbd43956ec2512b5e31fe1d18f9d669
Potter <472434557@qq.com>
2019-09-28 14:22:55 +0800

    msd radix sort ok

--- w9_4_MSDRadixSort/w9_4_MSDRadixSort.cpp
+++ w9_4_MSDRadixSort/w9_4_MSDRadixSort.cpp
@@ -15,38 +15,51 @@ int charAt(string str, int i)
 }
 
 // Most-significant-digit-first string sort
+void sort(vector<string>& vecStr, int lo, int hi, int w)
+{
+	if (hi < lo)
+		return;
+
+	int R = 256;
+	int d = w;
+
+	vector<int> r_count;
+	r_count.resize(R + 2, 0);
+
+	// count
+	for (int i = lo; i <= hi; i++)
+	{
+		r_count[charAt(vecStr[i], d) + 2]++;
+	}
+
+	// index
+	for (size_t i = 0; i < R + 1; i++)
+	{
+		r_count[i + 1] += r_count[i];
+	}
+
+	// arrange
+	vector<string> aa;
+	aa.resize(vecStr.size());
+	for (int i = lo; i <= hi; i++)
+	{
+		aa[r_count[charAt(vecStr[i], d) + 1]++] = vecStr[i];
+	}
+
+	// copy
+	for (int i = lo; i <= hi; i++)
+		vecStr[i] = aa[i-lo]; //第一轮OK，接下来对子序列进行排序
+
+	for (int r = 0; r < R; r++)
+	{
+		sort(vecStr, lo + r_count[r], lo + r_count[r + 1] - 1, d + 1);
+	}
+}
+
+// Most-significant-digit-first string sort
 void KeyIndexCount(vector<string>& vecStr, int w)
 {
-	int R = 256;
-
-	for (int d = 0; d < w; d++)
-	{
-		vector<int> r_count;
-		r_count.resize(R + 2, 0);
-
-		// count
-		for (size_t i = 0; i < vecStr.size(); i++)
-		{
-			r_count[charAt(vecStr[i], d) + 2]++;
-		}
-
-		// index
-		for (size_t i = 0; i < R+1; i++)
-		{
-			r_count[i + 1] += r_count[i];
-		}
-
-		// arrange
-		vector<string> aa;
-		aa.resize(vecStr.size());
-		for (size_t i = 0; i < vecStr.size(); i++)
-		{
-			aa[r_count[charAt(vecStr[i], d) + 1]++] = vecStr[i];
-		}
-
-		// copy
-		vecStr = aa; //第一轮OK，接下来对子序列进行排序
-	}
+	sort(vecStr, 0, w-1, 0);
 }
 
 int main()
