aae80edce289f6f93aa034d620037c9bd2f8ef9c
Potter <472434557@qq.com>
2020-03-25 00:09:32 +0800

    KMP code, may ok, need check again!

--- w10_4_Substring_KMP_MyThought/w10_4_Substring_KMP_MyThought.cpp
+++ w10_4_Substring_KMP_MyThought/w10_4_Substring_KMP_MyThought.cpp
@@ -7,33 +7,43 @@
 #include <cassert>
 using namespace std;
 
-int calcLongestFixed(string strCur, string &pattern, int i)
-{	
-	int N = strCur.length();
-	if (i > N)
-		return 0;
+// 我的计算方法
+int calcLongestFixed(string strMismatch, string pattern, int max_index) {	
+	if (max_index < 1)
+		return -1;
 
-	string strsub = strCur.substr(0, i-1);
-	string patternPrefix = pattern.substr(N-i);
-	if (strsub.substr(0, i-2) == patternPrefix.substr(0, i-2)
-		&& strsub[i-1] != patternPrefix[i-1])
-	{
-		return strsub.length();
+	int subpos = strMismatch.length() - max_index;
+	// 从最长的子字符串开始，进行匹配	
+	string subSuffix = strMismatch.substr(subpos, max_index);
+	string strPrefix = pattern.substr(0, max_index);
+
+	int M = subSuffix.length();	
+
+	string sub_true_suffix = subSuffix.substr(0, M - 1);
+	string sub_true_prefix = strPrefix.substr(0, M - 1);
+
+	char pos_i_char = strPrefix[M - 1]; // 新位置
+	char pos_j_char = subSuffix[M - 1]; // 原失配处	
+
+	// 找到pat[1, i - 1] = pat[j - i + 1, j - 1],并满足
+	// pat[i] != pat[j]的情况
+	if (sub_true_suffix.compare(sub_true_prefix) == 0
+		&& pos_i_char != pos_j_char){
+		return sub_true_suffix.length();
+	} else {
+		return calcLongestFixed(strMismatch, pattern, max_index - 1);
 	}
-	else
-		return calcLongestFixed(strCur, pattern, i + 1);
 }
 
-int calcLongestFixed(string strCur, string& pattern)
-{
-	return calcLongestFixed(strCur, pattern, 1);
+int calcLongestFixed(string strMismatch, string pattern ){
+	int i = strMismatch.length();
+	int max_index = i - 1;
+	return calcLongestFixed(strMismatch, pattern, max_index);
 }
 
-vector<int> InitVectorNext1(string& pattern)
-{
+vector<int> InitVectorNext_my_method(string& pattern) {
 	vector<int> vecNext;
-	for (int i = 0; i < pattern.length(); i++)
-	{
+	for (int i = 0; i < pattern.length(); i++) {
 		string substring = pattern.substr(0, i + 1);
 		int pos = calcLongestFixed(substring, pattern);
 
@@ -42,48 +52,48 @@ vector<int> InitVectorNext1(string& pattern)
 	return vecNext;
 }
 
-vector<int> InitVectorNext2(string &pattern)
+// 作者论文中所描述的方法
+vector<int> InitVectorNext_author_method(string &pattern)
 {
 	int N = pattern.length();
-	
 	vector<int> vecNext;
-	vecNext.resize(N);
-	vecNext[0] = 0;
+	vecNext.resize(N, -1); // 初始条件：vecNext[0] = 0，其他重新计算;
 
-	int j = 0;
-	for (int pos = 0; pos < N; pos++)
-	{
-		if (pattern[pos] == pattern[vecNext[j]])
-		{
-			vecNext[j + 1] = vecNext[j] + 1;
-		}
+	vector<int> f;
+	f.resize(N, -1); // 初始条件：vecF[0] = 0，其他重新计算;
+
+	for (int j = 0; j < N-1;) {
+		// 先找到pat[1,i-1]=pat[j-i+1,j-1]的情况
+		int t = f[j];
+		while (t >= 0 && pattern[j] != pattern[t])
+			t = vecNext[t];
+		f[j + 1] = t + 1;
+		j++;
+
+		// 判断pat[i]和pat[j]的情况
+		if (pattern[j] == pattern[f[j]])
+			vecNext[j] = vecNext[f[j]];
 		else
 		{
-			j = j - vecNext[j];
+			vecNext[j] = f[j];
 		}
 	}
 	return vecNext;
 }
 
-int search(string& strText, string& pattern, vector<int> &vecNext)
-{
+int search(string& strText, string& pattern, vector<int> &vecNext) {
 	int i = 0, j = 0;
 	int N = strText.length();
 	int M = pattern.length();
-	for (; i < N && j < M; i++)
-	{
-		if (strText[i] == pattern[j])
-		{
-			j++;			
-		}
-		else
-		{
-			j = 0;
-			i = i - vecNext[j];
+	for (; i < N && j < M;) {
+		if (j == -1 || strText[i] == pattern[j]) {
+			j++; i++;
+			if (j >= M)
+				return i - M;
+		} else {
+			j = vecNext[j];
 		}
 	}
-	if (j == M)
-		return i - M;
 	return -1;
 }
 
@@ -91,71 +101,65 @@ void testCalcLongestFixed();
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
 	testCalcLongestFixed();
-
-	string patter_ryf = "ABCDABD";
-	vector<int> vecNextRYF = InitVectorNext1(patter_ryf);
 	
-	string txt1 = "aabracadabra abacadabrabracabracadabrabrabracad";
-	string pattern1 = "abracadabra";	
-	cout << "===========================" << endl;
-	vector<int> vecNext = InitVectorNext1(pattern1);
-	cout << search(txt1, pattern1, vecNext) << endl;
+	/////////////////////////////////////////////////////////////////
+	cout << "test 1" << endl;
+	cout << "Expected: -1 0 0 0 -1 0 2" << endl;
+	string patter_ryf = "ABCDABD";
+	vector<int> vecNextRYF = InitVectorNext_author_method(patter_ryf);
+	for (int i = 0; i < vecNextRYF.size(); i++) {
+		cout << vecNextRYF[i] << " ";
+	}
+	cout << endl;
+	/////////////////////////////////////////////////////////////////
+	cout << "test 2" << endl;
+	cout << "Expected: -1 0 0 -1 0 0 -1 4 -1 0" << endl << "Actual: ";
+	string pattern_paper = "abcabcacab";
+	vector<int> vecNext_author = InitVectorNext_author_method(pattern_paper);
+	for (int i = 0; i < vecNext_author.size(); i++) {
+		cout << vecNext_author[i] << " ";
+	}
+	cout << endl;
+	/////////////////////////////////////////////////////////////////
+	{
+		cout << "========My method============" << endl;
+		string txt1 = "aabracadabra abacadabrabracabracadabrabrabracad";
+		string pattern1 = "abracadabra";
+		cout << "===========================" << endl;
+		vector<int> vecNext = InitVectorNext_my_method(pattern1);
+		cout << search(txt1, pattern1, vecNext) << endl;
 
-	string txt2 = "abacadabrabracabracadabrabrabracad";
-	string pattern2 = "rab";
-	cout << "===========================" << endl;
-	vector<int> vecNext2 = InitVectorNext1(pattern2);
-	cout << search(txt2, pattern2, vecNext2) << endl;
+		string txt2 = "abacadabrabracabracadabrabrabracad";
+		//string txt2 = "rrabasdsfsdasdfra";
+		string pattern2 = "rab";
+		cout << "===========================" << endl;
+		vector<int> vecNext2 = InitVectorNext_my_method(pattern2);
+		cout << search(txt2, pattern2, vecNext2) << endl;
+	}	
+	{
+		cout << "========Author's method============" << endl;
+		string txt1 = "aabracadabra abacadabrabracabracadabrabrabracad";
+		string pattern1 = "abracadabra";
+		cout << "===========================" << endl;
+		vector<int> vecNext = InitVectorNext_author_method(pattern1);
+		cout << search(txt1, pattern1, vecNext) << endl;
+
+		string txt2 = "rrarabasdsfsdasdfra";
+		string pattern2 = "rab";
+		cout << "===========================" << endl;
+		vector<int> vecNext2 = InitVectorNext_author_method(pattern2);
+		cout << search(txt2, pattern2, vecNext2) << endl;
+	}	
 }
 
 void testCalcLongestFixed()
 {
 	string pattern = "aaabc";
-	string s1 = "aaa"; // aaax处失配
-	assert(calcLongestFixed(s1, pattern) == 3);
+	string s1 = "aaac"; // aaax处失配
+	assert(calcLongestFixed(s1, pattern) == 2);
 
-	string s2 = "aaab"; // aaabx处失配
-	assert(calcLongestFixed(s2, pattern) == 4);
-
-	// 《算法：第四版》第497页的例子
-	string  pattern2 = "ABABAC";
-	{
-		string s3 = "A";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 1));
-	}
-	{
-		string s3 = "B";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 0));
-	}
-	{
-		string s3 = "C";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 0));
-	}
-	{
-		string s3 = "AA";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 1));
-	}
-	{
-		string s3 = "AB";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 2));
-	}
-	{
-		string s3 = "AC";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 0));
-	}
-	{
-		string s3 = "ABAB";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 4));
-	}
-	{
-		string s3 = "ABABB";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 0));
-	}
-	{
-		string s3 = "ABABAB";
-		assert((__LINE__, calcLongestFixed(s3, pattern2) == 4));
-	}
+	string s2 = "aaabd"; // aaabx处失配
+	cout << calcLongestFixed(s2, pattern);
+	assert(calcLongestFixed(s2, pattern) == 0);
 }
